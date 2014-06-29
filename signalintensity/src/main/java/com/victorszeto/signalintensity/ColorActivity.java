2001488bdf5eebf4c1327a3d55ec4efb14359a3d
Victor Szeto <me@victorszeto.com>
2014-06-29 00:28:46 -0400

    set text to RSSI value onReceive

--- signalintensity/src/main/java/com/victorszeto/signalintensity/ColorActivity.java
+++ signalintensity/src/main/java/com/victorszeto/signalintensity/ColorActivity.java
@@ -1,21 +1,20 @@
 package com.victorszeto.signalintensity;
 
-import com.victorszeto.signalintensity.WifiReceiver;
-
 import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.os.Handler;
+import android.widget.TextView;
+
 public class ColorActivity extends Activity {
 
-    WifiManager wm;
     WifiReceiver wr;
     IntentFilter rssiFilter = new IntentFilter(WifiManager.RSSI_CHANGED_ACTION);
 
@@ -25,12 +24,17 @@ public class ColorActivity extends Activity {
 
         setContentView(R.layout.activity_color);
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
+        View controlsView = findViewById(R.id.fullscreen_content_controls);
+        TextView contentView = (TextView) findViewById(R.id.fullscreen_content);
+
 
         wr = new WifiReceiver();
         this.registerReceiver(wr, rssiFilter);
+    }
 
+    public void setContentText(String msg) {
+        TextView rssiText = (TextView) findViewById(R.id.fullscreen_content);
+        rssiText.setText(msg);
     }
 
     @Override
@@ -44,19 +48,28 @@ public class ColorActivity extends Activity {
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        mInst = null;
         this.unregisterReceiver(wr);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        mInst = this;
+        this.registerReceiver(wr, rssiFilter);
+    }
 
+    private static ColorActivity mInst;
+
+    public static ColorActivity getInstance() {
+        return mInst;
     }
 }
 
--- signalintensity/src/main/java/com/victorszeto/signalintensity/WifiReceiver.java
+++ signalintensity/src/main/java/com/victorszeto/signalintensity/WifiReceiver.java
@@ -1,17 +1,23 @@
 package com.victorszeto.signalintensity;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
-import android.widget.Toast;
+import android.os.Handler;
 
 public class WifiReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        ColorActivity instance = ColorActivity.getInstance();
         int newRssi = intent.getIntExtra(wm.EXTRA_NEW_RSSI, 0);
-        System.out.println("recieved one "+newRssi);
+
+        if(instance != null) {
+            instance.setContentText(String.valueOf(newRssi));
+        }
+        else {
+            System.out.println("No activity detected. RSSI = "+newRssi);
+        }
     }
 }
