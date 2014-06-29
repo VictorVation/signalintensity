31c298c915cc9871b80310c8a878a2b4dfef4ecf
Victor Szeto <me@victorszeto.com>
2014-06-29 10:09:12 -0400

    add method to hide status/nav bar on touch

--- signalintensity/src/main/java/com/victorszeto/signalintensity/ColorActivity.java
+++ signalintensity/src/main/java/com/victorszeto/signalintensity/ColorActivity.java
@@ -2,6 +2,7 @@ package com.victorszeto.signalintensity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.net.wifi.WifiManager;
@@ -15,18 +16,32 @@ import android.widget.TextView;
 
 public class ColorActivity extends Activity {
 
+    private static ColorActivity mInst;
     WifiReceiver wr;
     IntentFilter rssiFilter = new IntentFilter(WifiManager.RSSI_CHANGED_ACTION);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_color);
 
-        View controlsView = findViewById(R.id.fullscreen_content_controls);
         TextView contentView = (TextView) findViewById(R.id.fullscreen_content);
 
+        final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp (MotionEvent e) {
+                setImmersive();
+                return true;
+            }
+        });
+
+        contentView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
 
         wr = new WifiReceiver();
         this.registerReceiver(wr, rssiFilter);
@@ -37,18 +52,22 @@ public class ColorActivity extends Activity {
         rssiText.setText(msg);
     }
 
+    private void setImmersive() {
+        ViewGroup currentView = (ViewGroup)getWindow().getDecorView();
+        currentView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            ViewGroup currentView = (ViewGroup)getWindow().getDecorView();
-            currentView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            setImmersive();
         }
     }
 
@@ -65,8 +84,6 @@ public class ColorActivity extends Activity {
         mInst = this;
         this.registerReceiver(wr, rssiFilter);
     }
-
-    private static ColorActivity mInst;
 
     public static ColorActivity getInstance() {
         return mInst;
--- signalintensity/src/main/java/com/victorszeto/signalintensity/WifiReceiver.java
+++ signalintensity/src/main/java/com/victorszeto/signalintensity/WifiReceiver.java
@@ -7,11 +7,11 @@ import android.net.wifi.WifiManager;
 import android.os.Handler;
 
 public class WifiReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         ColorActivity instance = ColorActivity.getInstance();
-        int newRssi = intent.getIntExtra(wm.EXTRA_NEW_RSSI, 0);
+        int newRssi = intent.getIntExtra(WifiManager.EXTRA_NEW_RSSI, 0);
 
         if(instance != null) {
             instance.setContentText(String.valueOf(newRssi));
